a3220f5252e1d1d7d45c074999e7c82a1a1da82b
brianmuturi2 <brianmuturi2@gmail.com>
2022-11-10 14:14:06 +0300

    feat(header): added services menu

--- src/components/ui/Header/Header.js
+++ src/components/ui/Header/Header.js
@@ -6,6 +6,8 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Button from '@mui/material/Button';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 import styles from './Header.module.css';
 import logo from '../../../assets/logo.svg'
@@ -35,14 +37,25 @@ function Header(props) {
 
     const [tabValue, setValue] = useState(0);
 
-    const handleChange = (event, newValue) => {
+    const handleTabChange = (event, newValue) => {
         setValue(newValue);
+    };
+
+    const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+    const menuOpen = Boolean(menuAnchorEl);
+    const handleMenuClick = (event) => {
+        setMenuAnchorEl(event.currentTarget);
+        setValue(1)
+    };
+    const handleMenuClose = () => {
+        setMenuAnchorEl(null);
     };
 
     useEffect(() => {
        if (window.location.pathname === '/' && tabValue !== 0) {
            setValue(0)
-       } else if (window.location.pathname === '/services' && tabValue !== 1) {
+       } else if (
+           (window.location.pathname === '/services' || window.location.pathname === '/custom-software' || window.location.pathname === '/mobile-apps' || window.location.pathname === '/websites') && tabValue !== 1) {
            setValue(1)
        } else if (window.location.pathname === '/revolution' && tabValue !== 2) {
            setValue(2)
@@ -58,14 +71,34 @@ function Header(props) {
             <AppBar position={'fixed'} color={'primary'}>
                 <Toolbar disableGutters>
                     <img src={logo} alt="company logo" className={styles.logo}/>
-                    <Tabs aria-label="navigation tabs" value={tabValue} onChange={handleChange} textColor="secondary" indicatorColor="primary" className={styles.tabContainer}>
+                    <Tabs aria-label="navigation tabs" value={tabValue} onChange={handleTabChange} textColor="secondary" indicatorColor="primary" className={styles.tabContainer}>
                         <Tab label="Home" {...a11yProps(0)} className={styles.tab} component={Link} to={'/'}/>
-                        <Tab label="Services" {...a11yProps(1)} className={styles.tab} component={Link} to={'services'}/>
+                        <Tab label="Services" {...a11yProps(1)} className={styles.tab}
+                             id="services-button"
+                             aria-controls={menuOpen ? 'services-menu' : undefined}
+                             aria-haspopup="true"
+                             aria-expanded={menuOpen ? 'true' : undefined}
+                             onMouseOver={handleMenuClick}/>
                         <Tab label="The Revolution" {...a11yProps(2)} className={styles.tab} component={Link} to={'revolution'}/>
                         <Tab label="About Us" {...a11yProps(3)} className={styles.tab} component={Link} to={'about'} />
                         <Tab label="Contact Us" {...a11yProps(4)} className={styles.tab} component={Link} to={'contact'}/>
                     </Tabs>
                     <Button variant="contained" color={'secondary'} className={styles.estimate} component={Link} to={'estimate'}>Free Estimate</Button>
+                    <Menu
+                        id="services-menu"
+                        anchorEl={menuAnchorEl}
+                        open={menuOpen}
+                        onClose={handleMenuClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'services-button',
+                            onMouseLeave: handleMenuClose
+                        }}
+                    >
+                        <MenuItem onClick={handleMenuClose} component={Link} to={'/services'}>Services</MenuItem>
+                        <MenuItem onClick={handleMenuClose} component={Link} to={'/custom-software'}>Custom Software Development</MenuItem>
+                        <MenuItem onClick={handleMenuClose} component={Link} to={'/mobile-apps'}>Mobile App Development</MenuItem>
+                        <MenuItem onClick={handleMenuClose} component={Link} to={'/websites'}>Website Development</MenuItem>
+                    </Menu>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
