0835ec98bc3aa05fd15a43e68e7e0088d5574108
brianmuturi2 <brianmuturi2@gmail.com>
2022-11-16 10:40:34 +0300

    feat(header): added hero animation

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,27 @@
+import React from 'react'
+import Lottie from 'react-lottie';
+
+import animationData from '../../animations/landinganimation/data'
+
+import styles from './Home.module.css'
+
 function Home() {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
     return (
-        <div>Home</div>
+        <div>
+            <Lottie options={defaultOptions}
+                    height={'100%'}
+                    width={'100%'}/>
+        </div>
     )
 }
 
