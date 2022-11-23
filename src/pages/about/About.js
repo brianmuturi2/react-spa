a096b28b4f6a90a1acaa930cc70331e58740d108
brianmuturi2 <brianmuturi2@gmail.com>
2022-11-23 16:31:59 +0300

    feat(about us): added about us page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,6 +1,62 @@
+import React from 'react';
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import {useTheme} from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import styles from './About.module.css';
+
+import history from '../../assets/history.svg';
+
 function About() {
+
+    const theme = useTheme();
+    const matchesMdDevice = useMediaQuery(theme.breakpoints.down('md'));
+
     return (
-        <div>About</div>
+        <Grid container direction={'column'} className={styles.mainContainer}>
+            <Grid item container alignItems={matchesMdDevice ? 'inherit' : 'center'}>
+                <Grid item container direction={'column'} className={styles.widthContainer} md>
+                    <Grid item>
+                        <Typography variant={'h4'} className={styles.heading4} gutterBottom>About Us</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant={'body1'} paragraph className={styles.body1}>
+                            Whether it be person to person, business to consumer, or an individual to their interests, technology is meant to bring us closer to what we care about in the best way possible. Arc Development will use that principle to provide fast, modern, inexpensive, and aesthetic software to the Midwest and beyond.
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item container className={styles.rowContainer} alignItems={matchesMdDevice ? 'inherit' : 'center'}>
+                <Grid item container direction={'column'} className={styles.widthContainer} md>
+                    <Grid item>
+                        <Typography variant={'h4'} className={styles.heading4} gutterBottom>History</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant={'body1'} paragraph className={styles.body1}>
+                            We're the new kid on the block.
+                        </Typography>
+                        <Typography variant={'body1'} paragraph className={styles.body1}>
+                            Founded in 2019, we're ready to get our hands on the world's business problems.
+                        </Typography>
+                        <Typography variant={'body1'} paragraph className={styles.body1}>
+                            It all started with one question: why aren't all businesses using available technology? <br/>There are many different answers to that question: economic barriers, social barriers, education barriers, and sometimes institutional barriers.
+                        </Typography>
+                        <Typography variant={'body1'} paragraph className={styles.body1}>
+                           We aim to be a powerful force in overcoming these obstacles. Recent developments in software engineering and computing power, compounded by the proliferation of smart phones, has opened up infinite worlds of possibility. Things that have always been done by hand can now be done digitally and automatically, and completely new methods of interaction are created daily. Taking full advantage of these advancements is the name of the game.
+                        </Typography>
+                        <Typography variant={'body1'} paragraph className={styles.body1}>
+                            All this change can be a lot to keep up with, and that's where we come in.
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid item md>
+                    <img src={history} alt="quill pen sitting on top of book" className={styles.bookIcon} style={{marginLeft: matchesMdDevice ? 0 : '5em'}}/>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
--- src/pages/revolution/Revolution.js
+++ src/pages/revolution/Revolution.js
@@ -111,13 +111,13 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Our process begins the moment you realize you need a piece of technology for your business. Whether you already have an idea for where to start and what to do, or if you just know you want to step things up, our initial consultation will help you examine your business holistically to find the best solutions.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Detailed notes will be taken on your requirements and constraints, while taking cate to identify areas for consideration.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Cutting-edge advancements in machine learning like object detection and natural language processing allow computers to do things previously unimaginable, and our expertise and solution will help usher you into this new future of possibilities.
                                 </Typography>
                             </Grid>
@@ -136,13 +136,13 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     After we settle on the best path forward and decide on a solution to pursue, details like the cost and timeline will be finalized.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Then it's time for us to start on your minimum viable product. That's just a fancy term for a mockup, which doesn't include colors, images, or any other polished design elements, but captures the essential layout structure and functionality.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     This helps us understand and refine the solution itself before getting distracted by specifics and looks.
                                 </Typography>
                             </Grid>
@@ -161,13 +161,13 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Before moving any farther we come back to you with our progress. This gives you the freedom to discuss any changes you may want or any ideas you may have come up with before any heavy lifting has been done.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     We give you an interactive demonstration of the mockups, thoroughly explaining the thought process that went into each screen and every anticipated feature.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Once you're completely satisfied with the vision for our solution we get down to the nitty gritty, fine-details of design.
                                 </Typography>
                             </Grid>
@@ -186,10 +186,10 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Using the mockups and notes taken during the consultation as guides, we will start ironing out what the final product will look like. This also involves using any brand material like fonts, colors, and logos to extend the experience you're already familiar with.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     No aspect is superfluous, and care will be taken with every decision.
                                 </Typography>
                             </Grid>
@@ -208,10 +208,10 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     A second round of review is essential to our goal of creating exactly what you want, exactly how you want it.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     This time we'll be going over the finalized designs in another fully interactive demonstration. Again this gives you an opportunity to tweak things and make sure we get everything right the first time.
                                 </Typography>
                             </Grid>
@@ -230,22 +230,22 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Here's where we get down to business.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Engineering begins after your approval on the final designs. We start by scaffolding out the project on a high level, prioritizing some areas over others.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Each area is then developed in order of importance until ready to be connected to the next place.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Typically the backend, behind the scenes operations are completed first. Once all the services are in place we can then create the front end user side of things.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Finishing the application doesn't mean we're done though, because we use extensive testing to guarantee compatibility with all intended devices.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Only after our rigorous examinations will we accept a product as finished, the pushing it through the production pipeline. This produces an optimized, compressed, consumer version of the code and assets ready for deployment.
                                 </Typography>
                             </Grid>
@@ -264,13 +264,13 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     The moment we've all been waiting for.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     When construction comes to a close you're the first one to know. We'll give our final demonstration to show off your shiny new software in the wild so you know exactly how it will look to your users.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     When you say the word, we press the button and launch your project out to the public. We're there to ensure everything goes to plan so you can start reaping the rewards of your technological investment immediately.
                                 </Typography>
                             </Grid>
@@ -289,13 +289,13 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Our work doesn't end there.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     After a successful launch we keep in close contact to listen to feedback and hear how the project is being received.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     From there on out we make sure your application is kept up to date and taking advantage of the best features and practices available. When new developments arise or new techniques are discovered in future projects, we will implement those advancements in your project as part of our routine maintenance.
                                 </Typography>
                             </Grid>
@@ -314,13 +314,13 @@ function Revolution() {
                         </Grid>
                         <Grid item container>
                             <Grid item>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     The cycle repeats whenever you come up with a new idea for extending your current project, or come up with a brand new system entirely.
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     By planning for future features and changes we can build and evolve your application over time. As new use cases and customer needs develop we can respond with continuous integration of new content
                                 </Typography>
-                                <Typography variant={'body1'} paragraph>
+                                <Typography variant={'body1'} paragraph className={styles.processTextBody}>
                                     Our iterative process will keep you current and competitive, allowing you to quickly implement changes instead of waiting months for a single update.
                                 </Typography>
                             </Grid>
